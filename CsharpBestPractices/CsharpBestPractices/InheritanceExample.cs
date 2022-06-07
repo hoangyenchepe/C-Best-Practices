f06ad43132956d6baf393856618023f1eabcda40
YenVoChePe <yen.vo@tekcent.com>
2022-06-07 15:46:39 +0700

    Inheritance

---
+++ CsharpBestPractices/CsharpBestPractices/InheritanceExample.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections;
+
+namespace CsharpBestPractices
+{
+    class InheritanceExample
+    {
+        public void features()
+        {
+            ArrayList my_features = new ArrayList();
+
+            my_features.Add("Abstraction");
+            my_features.Add("Encapsulation");
+            my_features.Add("Inheritance");
+
+
+            Console.WriteLine("Features provided by OOPS:");
+            foreach (var elements in my_features)
+            {
+                Console.WriteLine(elements);
+            }
+        }
+    }
+
+    class Inheritance2 : InheritanceExample
+    {
+        public void languages()
+        {
+            ArrayList my_features = new ArrayList();
+
+            my_features.Add("C++");
+            my_features.Add("C#");
+            my_features.Add("JScript");
+
+            Console.WriteLine("\nLanguages that use OOPS concepts:");
+            foreach (var elements in my_features)
+            {
+                Console.WriteLine(elements);
+            }
+        }
+    }
+
+    class InheritanceToInheritance : Inheritance2 { }
+}
